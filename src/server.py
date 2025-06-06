d9b2e8d0ec64d325e9331a1cb5fda460a135332f
friend1226 <friendcha7749@gmail.com>
2025-06-06 11:43:31 +0900

    [feat] Get image ID from body in upload method

--- src/server.py
+++ src/server.py
@@ -4,6 +4,8 @@ import requests
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
+from pydantic import BaseModel
+
 from .core import Model
 from .pincone_db import PineconeDB, API_KEY, ENVIRONMENT, INDEX_NAME
 
@@ -16,19 +18,23 @@ db = PineconeDB(API_KEY, ENVIRONMENT, INDEX_NAME)
 s3_URL = os.getenv("S3_URL")
 SPECIAL_KEY = os.getenv("SPECIAL_KEY")  # Temporary authentication key
 
+class UploadItem(BaseModel):
+    image_id: str
+
 @app.get("/api/count")
 def api_get_uploaded_images():
     return JSONResponse(content={"count": db.count()})
 
 @app.post("/api/upload")
-def api_upload_image(filename: str, image_id: str):
+def api_upload_image(item: UploadItem):
+    image_id = item.image_id
     with requests.get(s3_URL + image_id) as response:
         if response.status_code != 200:
             return JSONResponse(content={"result": "failed", "error_msg": "Error on communicating to image server."})
         data = response.content
     vector = model.get_image_vector(data)
     try:
-        db.push(filename, vector)
+        db.push(image_id, vector)
     except ValueError as e:
         return JSONResponse(content={"result": "failed", "error_msg": f"{type(e)}: {e}"})
     return JSONResponse(content={"result": "success"})
