3463e446b755faaa87a5261ab48c0510e0974980
friend1226 <friendcha7749@gmail.com>
2025-12-01 17:43:43 +0900

    [feat] Get user_id when uploading

--- src/server.py
+++ src/server.py
@@ -44,7 +44,7 @@ db = PineconeDB(API_KEY, ENVIRONMENT, INDEX_NAME)
 s3_URL = os.getenv("S3_URL")
 SPECIAL_KEY = os.getenv("SPECIAL_KEY")  # Temporary authentication key
 
-def upload_to_db(image_id):
+def upload_to_db(user_id, image_id):
     timer = SimpleTimer()
     timer.start()
     with requests.get(s3_URL + image_id) as response:
@@ -59,7 +59,7 @@ def upload_to_db(image_id):
     logger.info(f"[{threading.get_ident()} - Upload] Computed feature vector of {image_id} ({elapsed:.3f} sec)")
     timer.start()
     try:
-        db.push(image_id, vector)
+        db.push(user_id, image_id, vector)
     except ValueError as e:
         return {"image_id": image_id, "status": "failed", "error_msg": f"{type(e)}: {e}"}
     elapsed = timer.stop()
@@ -74,7 +74,7 @@ def api_get_uploaded_images():
     return JSONResponse(content=content)
 
 @app.post("/api/upload")
-def api_upload_image(image_ids: list[str] = Body(...)):
+def api_upload_image(user_id: str, image_ids: list[str] = Body(...)):
     logger.info(f"[{threading.get_ident()}] ====== /api/upload ======", extra={"color_message": f"[{threading.get_ident()}] ====== {click.style('/api/upload', bold=True)} ======"})
     resp_json = {"status": {"success": 0, "failed": 0}, "results": []}
     timer = SimpleTimer()
@@ -82,7 +82,7 @@ def api_upload_image(image_ids: list[str] = Body(...)):
     subtimer = SimpleTimer()
     for image_id in image_ids:
         subtimer.start()
-        result = upload_to_db(image_id)
+        result = upload_to_db(user_id, image_id)
         elapsed = subtimer.stop()
         logger.info(f"[{threading.get_ident()}] Uploaded {image_id} to VecDB : {result.get('error_msg', 'Success')} ({elapsed:.3f} sec)")
         resp_json["results"].append(result)
